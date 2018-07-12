8e8db16e44a0f855c3c0d35df612612f20f6a4cf
Wangxinfeng <Wangxinfeng@zlzkj.com>
2018-07-12 09:25:02 +0800

    fixed

--- src/main/java/com/zlzkj/app/service/index/ShiroUserService.java
+++ src/main/java/com/zlzkj/app/service/index/ShiroUserService.java
@@ -1,32 +1,92 @@
-
-
 package com.zlzkj.app.service.index;
 
 import com.zlzkj.app.model.index.Authority;
 import com.zlzkj.app.model.index.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * Interface description
- *
- *
- * @version        1.0.0, 16/03/31
- * @author         zhm
- */
-public interface ShiroUserService {
-    User getAuthenticatedUser(AuthenticationToken authcToken);
 
-    User getLoginUser();
+@Service("shiroUserService")
+@Transactional
+public class ShiroUserService {
+    @Resource
+    private UserService userService;
+    
+    public User getAuthenticatedUser(AuthenticationToken authcToken) {
 
-    void setUserSessionPermission(List<Authority> permissions);
+        // TODO Auto-generated method stub
+        UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
+        //String md5password = Md5Util.getMD5(token.getUsername() + String.valueOf(token.getPassword()));
+        User user = userService.findByUserName(token.getUsername());
 
-    List<Authority> getUserSessionPermission();
+        if (user == null) {
+            throw new AuthenticationException("usernameorpassword.not.match");
+        } else if (!user.getPassword().equals(String.valueOf(token.getPassword()))) {
+            throw new AuthenticationException("usernameorpassword.not.match");
+        } else if ((user.getLocked())) {
+            throw new AuthenticationException("account.not.activate");
+        }
 
-    void logout ();
+        return user;
+    }
 
-    /*boolean checkPermission(String code);*/
+    
+    public User getLoginUser() {
+
+        // TODO Auto-generated method stub
+        org.apache.shiro.subject.Subject my = SecurityUtils.getSubject();
+
+        /*String username = (String) my.getPrincipal();
+
+        if (username == null) {
+            return null;
+        }*/
+
+        return (User)my.getPrincipal();
+    }
+
+    public boolean isSuperAdmin() {
+        return getLoginUser().getRoleId().equals("1");
+    }
+
+    public void setUserSessionPermission(List<Authority> permissions) {
+        org.apache.shiro.subject.Subject my = SecurityUtils.getSubject();
+        my.getSession(true).setAttribute("permissions",permissions);
+    }
+
+    public List<Authority> getUserSessionPermission() {
+        org.apache.shiro.subject.Subject my = SecurityUtils.getSubject();
+        return (List<Authority>) my.getSession().getAttribute("permissions");
+    }
+
+/*   
+    }*/
+
+	/*public List<String> getCode(String userid,String recordtypeid) {
+		return userService.getCode(userid,recordtypeid);
+	}*/
+
+    public void logout () {
+        SecurityUtils.getSubject().logout();
+    }
+
+/*
+	public boolean checkPermission(String code) {
+        org.apache.shiro.subject.Subject my = SecurityUtils.getSubject();
+        List<Authority> as = (List<Authority>) my.getSession().getAttribute("permissions");
+		 for(Authority a:as){
+	    	if(a.getCode().equals(code))
+	    		return true;
+		 }
+		return false;
+	}*/
 }
 
 
--- src/main/java/com/zlzkj/app/service/index/UserService.java
+++ src/main/java/com/zlzkj/app/service/index/UserService.java
@@ -1,25 +1,69 @@
 package com.zlzkj.app.service.index;
 
+import com.zlzkj.app.mapper.UserMapper;
 import com.zlzkj.app.model.Row;
 import com.zlzkj.app.model.index.User;
 import com.zlzkj.app.util.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
 
-public interface UserService {
-    public Integer delete(int id);
+//业务层接口实现
+@Service("userService")
+@Transactional
+public class UserService{
 
-    public Integer update(User entity);
 
-    public Integer save(User entity);
+    @Value("${page.size}")
+    private int PAGE_SIZE;
 
-    public User findById(int id);
+    @Autowired
+    private UserMapper mapper;
 
-    public List<Row> findByMap(Map<String,Object> map);
+    public User findById(int id ){
 
-    public Page findByMap(Map<String,Object> parmMap, Integer nowPage);
+        User user = new User(id,"测试");
+        mapper.updateByPrimaryKey(user);
+        if(id == 0){
+            throw new RuntimeException("ID 不能为 0 " );
+        }
+        return mapper.selectByPrimaryKey(id);
+    }
+    public Integer delete(int id){
+        try{
+            return mapper.deleteByPrimaryKey(id);
+        }catch (Exception e){
+            throw new RuntimeException("删除失败");
+        }
+    }
 
-    public User findByUserName(String username);
+    public Integer update(User entity){
+        return mapper.updateByPrimaryKey(entity);
+    }
 
+    public Integer save(User entity) {
+        return mapper.insert(entity);
+    }
+
+
+    public List<Row> findByMap(Map<String,Object> map){
+        return mapper.selectByMap(map);
+    }
+
+    public Page findByMap(Map<String,Object> parmMap, Integer nowPage){
+        if(nowPage == null) nowPage = 1;
+        parmMap.put("start",(nowPage-1)*PAGE_SIZE);
+        parmMap.put("end",PAGE_SIZE);
+        //PageHelper.startPage(1, 3);
+
+        return new Page(findByMap(parmMap),mapper.countByMap(parmMap),nowPage,PAGE_SIZE);
+    }
+
+    public User findByUserName(String username) {
+        return mapper.selectByUserName(username);
+    }
 }
